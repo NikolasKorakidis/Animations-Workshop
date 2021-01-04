919e270080ab7d97dd4adee9092a187eab14bd46
nikolaskorakidis <korakidisn@gmail.com>
2021-01-04 20:48:52 +0100

    added svg humburger and frame motion

--- src/components/FullScreenMenu.jsx
+++ src/components/FullScreenMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { motion } from "framer-motion";
 import "./FullscreenMenu.css";
 
 export default function FullscreenMenu() {
@@ -30,12 +30,32 @@ export default function FullscreenMenu() {
         onClick={() => setOpen(!open)}
       >
         <svg width="2rem" height="2rem" viewBox="0 0 24 24">
-          <g stroke="currentColor" strokeWidth={2}>
-            <line x1="0" y1="4" x2="24" y2="4" />
-            <line x1="0" y1="12" x2="24" y2="12" />
-            <line x1="0" y1="20" x2="24" y2="20" />
-          </g>
-        </svg>
+  <g stroke="currentColor" strokeWidth={2}>
+    <motion.line
+      x1="0" y1="4" x2="24" y2="4"
+      animate={{
+        x1: open ? 4 : 0,
+        x2: open ? 20 : 24,
+        y2: open ? 20 : 4
+      }}
+    />
+    <motion.line
+      x1="0" y1="12" x2="24" y2="12"
+      animate={{
+        x1: open ? 12 : 0,
+        x2: open ? 12 : 24
+      }}
+    />
+    <motion.line
+      x1="0" y1="20" x2="24" y2="20"
+      animate={{
+        x1: open ? 4 : 0,
+        x2: open ? 20 : 24,
+        y2: open ? 4 : 20
+      }}
+    />
+  </g>
+</svg>
       </button>
     </>
   );
